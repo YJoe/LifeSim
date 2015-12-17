da3004740de430e36327079bfc4b74391ffa3a6b
YJoe <YJoe762@gmail.com>
2015-12-17 10:49:49 +0000

    Hello World

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Group root = new Group();
-        primaryStage.setTitle("Hello Aaron");
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
